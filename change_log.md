fb7cdd4c580ff90444714d756e5d2af1923074ae
Mikhaila Friske <frisk028@umn.edu>
2019-03-18 22:09:27 -0600

    add code from testing

---
+++ change_log.md
@@ -0,0 +1,10 @@
+# Change Log
+
+## Gabe Chapel
+
+## Jack DeMay
+
+## Mikhaila Friske
+Microbit for keyboard work. I started trying to use the acceleration from the micro:bit to try and get the point where the it would know when you are moving down. However, because it is acceleration and not velocity it was not working as well as I hoped (or really at all). I started to play around with the events of the micro:bit (like on shake) and messing around with putting a delay so it would "flip a switch" when it is shook to trigger a class in wekinator.
+
+## Bridget Murphy
---
+++ mb_to_osc.py
@@ -0,0 +1,29 @@
+# requires the following python libraries:
+#  python-osc
+#  pyserial
+
+import serial
+import math
+from pythonosc import udp_client 
+
+# change this string depending upon where your computer makes a device for the micro:bit
+serialport = "/dev/cu.usbmodem1422"
+
+ser = serial.Serial(serialport, 115200)
+client = udp_client.SimpleUDPClient("localhost", 6448)
+
+while(True):
+    line = ser.readline().decode('utf8').strip()
+    
+    try:
+
+      x, y, z, direction, strength = map(float, line.split("^"))
+      acceleration = math.sqrt(x**2 + y**2 + z**2)
+
+      print(f"({acceleration}, {direction})")
+
+
+    #   client.send_message("/air_band/keyboard", [acceleration, strength] )
+    except:
+      print('here')
+      pass
