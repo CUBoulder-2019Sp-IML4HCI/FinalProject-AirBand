9352e8150a6e7ea7c3841ebd70ce0f9ad3ddb9e6
gabechapel <31941529+gabechapel@users.noreply.github.com>
2019-04-14 14:14:52 -0600

    Update keyboard.py

--- instruments/keyboard/keyboard.py
+++ instruments/keyboard/keyboard.py
@@ -11,18 +11,27 @@ serialport = "/dev/cu.usbmodem1422"
 ser = serial.Serial(serialport, 115200)
 client = udp_client.SimpleUDPClient("localhost", 8999)
 
+## initialize right and left
+right = [0,0,0,0]
+left = [0,0,0,0]
+
 while(True):
     line = ser.readline().decode('utf8').strip()
-    
+
     try:
-        finger1, finger2, finger3, finger4 = map(float, line.split("^"))
+        if line[0] == "R":
+            right = line[2:]
+        elif line[0] == "L":
+            left = line[2:]
+        finger1, finger2, finger3, finger4 = map(float, left.split("^"))
+        finger5, finger6, finger7, finger8 = map(float, right.split("^"))
 
-        print(f"({finger1}, {finger2}, {finger3}, {finger4})")
+        print(f"({finger1}, {finger2}, {finger3}, {finger4},{finger5}, {finger6}, {finger7}, {finger8})")
 #        print(x, y, z)
-#        print(line)
+#        print(line[0])
 
 
 
-        client.send_message("/wek/inputs", [finger1, finger2, finger3, finger4] )
+        client.send_message("/wek/inputs", [finger1, finger2, finger3, finger4,finger5, finger6, finger7, finger8] )
     except:
         pass
