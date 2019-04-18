85a59bbee87b66f14a9550edfb28d670d64f7a2d
Bridget Murphy <39529905+brmu8861@users.noreply.github.com>
2019-04-18 11:55:38 -0600

    Add files via upload
    New drum.py to read all 3 microbits

--- instruments/drums/drum.py
+++ instruments/drums/drum.py
@@ -3,26 +3,43 @@
 #  pyserial
 
 import serial
-import math
-from pythonosc import udp_client 
+from pythonosc import udp_client
 
 # change this string depending upon where your computer makes a device for the micro:bit
 serialport = "/dev/cu.usbmodem1412"
 
 ser = serial.Serial(serialport, 115200)
-client = udp_client.SimpleUDPClient("localhost", 6448)
+client = udp_client.SimpleUDPClient("localhost", 8999)
+
+## initialize right and left
+right = 0
+left = 0
+kick = 0
 
 while(True):
     line = ser.readline().decode('utf8').strip()
-    
-    try:
 
-      direction = float(line)
-      
-      print(f"({direction})")
-      
-      client.send_message("/air_band/drum", [direction] )
+    try:
+        if line[0] == "R":
+            right = line[2:]
+        elif line[0] == "L":
+            left = line[2:]
+        elif line[0] == "K":
+            kick = line[2:]
+
+        # lef = map(float, left)
+        # rig = map(float, right)
+        # kick = map(float, kick)
+        left = float(left)
+        right = float(right)
+        kick = float(kick)
+
+        print(f"({left}, {right}, {kick}")
+#        print(x, y, z)
+       # print(line)
+
+
+
+        # client.send_message("/wek/inputs", [lef, rig, kick] )
     except:
-      # print(line)
-      # print('here')
-      pass
+        pass
